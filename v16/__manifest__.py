f03d879472a0e7e4b681f74d03bf560442abde37
Polpo-Test5 <polpo.test5@gmail.com>
2024-08-30 10:28:22 -0300

    Update __manifest__.py

--- v16/__manifest__.py
+++ v16/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Nombre del Módulo',
-    'version': '13.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Categoría',
     'summary': 'Resumen de las características del módulo.',
     'description': 'Descripción detallada del módulo.',
@@ -13,4 +13,6 @@
     ],
     'installable': True,
     'application': True,
+    'price': 49.99,
+    'currency': 'USD',
 }
