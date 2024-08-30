6082d92e3f1a91747f37a002627e9544f32036e0
Polpo-Test5 <polpo.test5@gmail.com>
2024-08-30 10:25:04 -0300

    Create __manifest__.py

---
+++ v13/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Nombre del Módulo',
+    'version': '13.0.1.0.0',
+    'category': 'Categoría',
+    'summary': 'Resumen de las características del módulo.',
+    'description': 'Descripción detallada del módulo.',
+    'author': 'Polpo',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
